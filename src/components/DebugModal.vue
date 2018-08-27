57139fd68aacca8312e9b5557c229b24854191a1
Peter Stuifzand <peter@stuifzand.eu>
2018-08-27 23:19:21 +0200

    Add missing DebugModal

---
+++ src/components/DebugModal.vue
@@ -0,0 +1,36 @@
+<template>
+  <div :class="classes">
+    <div class="modal-background"></div>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <p class="modal-card-title">Debug</p>
+        <button class="delete" aria-label="close" @click="close"></button>
+      </header>
+      <section class="modal-card-body"><pre>{{ JSON.stringify(item, null, 2) }}</pre></section>
+      <footer class="modal-card-foot">
+        <button class="button" @click="close">Cancel</button>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "DebugModal",
+    props: ['active', 'item'],
+    computed: {
+      classes() {
+        return {'is-active': this.active, 'modal': true}
+      }
+    },
+    methods: {
+      close() {
+        this.$emit('close')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
